87c7dcfc5ef4852dc6935e7a05d67949a40e6e62
OKE225 <120661050+OKE225@users.noreply.github.com>
2023-08-02 18:31:19 +0200

    Creating a function that adds products to favorites & to the shopping cart, changing menu filters checkbox to buttons

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const heartBtn = document.querySelectorAll(".button-heart");
+const heartFavNum = document.getElementById("num-add-heart");
+
 const cartBtn = document.querySelectorAll(".button-cart-shopping");
+const cartAddNum = document.getElementById("num-add-cart");
+
+const filterBtn = document.querySelectorAll(".menu-filter-btn");
 
 heartBtn.forEach(function (heartBtn) {
   var switchButton = 1;
   heartBtn.addEventListener("click", function (event) {
     if (switchButton === 1) {
       event.preventDefault();
-      heartBtn.classList.add("active-heart-btn");
-      heartBtn.classList.remove("btn-outline-danger");
+      heartBtn.classList.add("active");
+      ++heartFavNum.innerHTML;
       switchButton = 2;
     } else if (switchButton === 2) {
       event.preventDefault();
-      heartBtn.classList.add("btn-outline-danger");
-      heartBtn.classList.remove("active-heart-btn");
+      heartBtn.classList.remove("active");
+      --heartFavNum.innerHTML;
       switchButton = 1;
     }
   });
@@ -21,12 +26,25 @@ heartBtn.forEach(function (heartBtn) {
 cartBtn.forEach(function (cartBtn) {
   cartBtn.addEventListener("click", function (event) {
     event.preventDefault();
-    cartBtn.classList.add("click-cart-shop-btn");
-    cartBtn.classList.remove("btn-outline-primary");
-
+    // cartBtn.classList.add("click-cart-shop-btn");
+    cartBtn.classList.add("active");
+    ++cartAddNum.innerHTML;
     setTimeout(() => {
-      cartBtn.classList.add("btn-outline-primary");
-      cartBtn.classList.remove("click-cart-shop-btn");
+      // cartBtn.classList.remove("click-cart-shop-btn");
+      cartBtn.classList.remove("active");
     }, 250);
+  });
+});
+
+filterBtn.forEach(function (filterBtn) {
+  var switchButton = 1;
+  filterBtn.addEventListener("click", function (event) {
+    if (switchButton === 1) {
+      filterBtn.classList.add("active");
+      switchButton = 2;
+    } else if (switchButton === 2) {
+      filterBtn.classList.remove("active");
+      switchButton = 1;
+    }
   });
 });
